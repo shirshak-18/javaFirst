bcc6e7797d35adaeca3f9c82cbfaeebba2e85afa
shirshak_18 <shirshakdixit18@gmail.com>
2024-09-02 22:46:12 +0530

    first commit

--- java/firstprog.java
+++ java/firstprog.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
 
-public class firstprog {
-    public static void main(String[] args) {
-      int n1 = factor();
-      if (n1==2) {
-         System.out.println("the number is prime");
+public class FirstProg {
+
+   public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      System.out.println("enter the array size");
+      int n = scn.nextInt();
+      int [] arr = new int [n];
+      System.out.println("enter the array elements");
+      for(int i = 0; i<n; i++) {
+            arr[i] = scn.nextInt();
       }
-      else System.out.println("the number is composite");
+      int m = max(arr); 
+      System.out.println(count(arr, m));
+      
 }
-   public static int factor() {
-      Scanner scn = new Scanner (System.in);
-      int n = scn.nextInt(); int count = 0;
-      for(int i = 1; i<=n; i++) {
-         if (n%i == 0) {
-         count++;
-         }
+public static int max(int [] arr) {
+      int max = arr[0];
+      for(int i = 1; i<arr.length; i++) {
+            if (arr[i] > max) {
+                  max = arr[i];
+            }
+      }
+      return max;
+}
+public static int count(int [] arr, int max ) {
+      int count = 0;
+      int m = max(arr);
+      for(int i = 0; i<arr.length; i++){
+            if (arr[i] < m) {
+                  count ++;
+            }
       }
       return count;
-   }
 }
+}
+      
+
+    
+   
+
